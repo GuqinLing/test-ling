b6988d56a9fc41af28b1e30077440cf8962a4ebe
SanBo Ling <38552685+GuqinLing@users.noreply.github.com>
2019-02-25 17:51:43 +0800

    Create hello.c

---
+++ test1/hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(int argc,char *argv[])
+{
+  char array[] = "i love china,how about you";
+  char buffer[] = "yes,i love china too.";
+  printf_s("Jack say : %s\n",array);
+  printf_s("Bob say : %s\n",buffer);
+  return 0;
+}
